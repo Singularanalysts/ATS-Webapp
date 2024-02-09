7c6fa2321388bc676697c0d2c83d7c1028951313
PraneethNarvee <praneethreddyjulakanti@narveetech.com>
2024-02-09 18:06:17 +0530

    dashboard component and service changes

--- src/app/usit/components/dashboard/dashboard.component.ts
+++ src/app/usit/components/dashboard/dashboard.component.ts
@@ -1,14 +1,208 @@
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { DashboardService } from 'src/app/usit/services/dashboard.service';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatTooltipModule,
+    MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatStepperModule,
+    MatMenuModule
+  ],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  dataSource = new MatTableDataSource([]);
+  dataSourceDice = new MatTableDataSource([]);
+  dataTableColumns: string[] = [
+    'Name',
+    'Category',
+    'Address',
+    'Email',
+    'ContactNumber',
+    'Owner',
+    'Track'
+  ];
+  dataTableColumnsDice: string[] = [
+    'PostedDate',
+    'JobTitle',
+    'Category',
+    'JobLocation',
+    'Vendor',
+    'TaggedDate',
+    'TaggedBy'
+  ];
+  entity: any;
+  datarr: any[] = [];
+  private dashboardServ = inject(DashboardService);
+  submissionCount: any;
+  closureFlag = 'Monthly';
+  interviewFlag = 'daily';
+  submissionFlag = 'Today';
+  subCountArr: any[] = [];
+  intCountArr: any[] = [];
+  closecountArr: [] = [];
+  subcount = 0;
+  reccount = 0;
+  sintcount = 0;
+  rintcount = 0;
+  sclosecount = 0;
+  rclosecount = 0;
+
+  ngOnInit(): void {
+    this.getDiceReqs();
+    this.getSourcingLeads();
+    this.dashboardServ.vmstransactions().subscribe(
+      ((response: any) => {
+        this.datarr = response.data;
+      })
+    );
+
+    this.dashboardServ.getsubmissionCount('daily').subscribe(
+      ((response: any) => {
+        this.subCountArr = response.data;
+        this.subCountArr.forEach((ent: any) => {
+          if (ent.salescount != null) {
+            this.subcount = ent.salescount;
+          }
+          if (ent.reccount != null) {
+            this.reccount = ent.reccount;
+          }
+        });
+      })
+    );
+
+    this.dashboardServ.getInterviewCount('daily').subscribe(
+      ((response: any) => {
+        this.intCountArr = response.data;
+        this.intCountArr.forEach((ent: any) => {
+          if (ent.salescount != null) {
+            this.sintcount = ent.salescount;
+          }
+          if (ent.reccount != null) {
+            this.rintcount = ent.reccount;
+          }
+        });
+      })
+    );
+
+    this.dashboardServ.getClosureCount('monthly').subscribe(
+      ((response: any) => {
+        this.closecountArr = response.data;
+        this.closecountArr.forEach((ent: any) => {
+          if (ent.salescount != null) {
+            this.sclosecount = ent.salescount;
+          }
+          if (ent.reccount != null) {
+            this.rclosecount = ent.reccount;
+          }
+        });
+      })
+    );
+  }
+
+  getDiceReqs() {
+    this.dashboardServ.getDiceRequirements().subscribe(
+      (response: any) => {
+        this.entity = response.data;
+        this.dataSourceDice.data = response.data;
+        // this.dataSourceDice.data.map((x: any, i) => {
+        //   x.serialNum = i + 1;
+        // });
+      }
+    )
+  }
+
+  userid = localStorage.getItem('userid');
+  getSourcingLeads() {
+    this.dashboardServ.getSourcingLeads(this.userid).subscribe(
+      (response: any) => {
+        this.entity = response.data;
+        this.dataSource.data = response.data;
+        // this.dataSource.data.map((x: any, i) => {
+        //   x.serialNum = i + 1;
+        // });
+      }
+    )
+  }
+
+  getSubmissionCount() {
+    this.dashboardServ.getsubmissionCount('weekly').subscribe((response: any) => {
+      this.submissionCount = response.data;
+    })
+  }
+
+  subCount(flag: string, flg: string) {
+    this.subcount = 0;
+    this.reccount = 0;
+    this.submissionFlag = flg;
+    this.dashboardServ.getsubmissionCount(flag).subscribe(
+      ((response: any) => {
+        this.subCountArr = response.data;
+        this.subCountArr.forEach((ent: any) => {
+          if (ent.salescount != null) {
+            this.subcount = ent.salescount;
+          }
+          if (ent.reccount != null) {
+            this.reccount = ent.reccount;
+          }
+        });
+      })
+    );
+  }
+
+  intCount(flag: string, flg: string) {
+    this.interviewFlag = flg;
+    this.sintcount = 0;
+    this.rintcount = 0;
+    this.dashboardServ.getInterviewCount(flag).subscribe(
+      ((response: any) => {
+        this.intCountArr = response.data;
+        this.intCountArr.forEach((ent: any) => {
+          if (ent.salescount != null) {
+            this.sintcount = ent.salescount;
+          }
+          if (ent.reccount != null) {
+            this.rintcount = ent.reccount;
+          }
+        });
+      })
+    );
+  }
+
+  closureCount(flag: string, flg: string) {
+    this.closureFlag = flg;
+    this.sclosecount = 0;
+    this.rclosecount = 0;
+    this.dashboardServ.getClosureCount(flag).subscribe(
+      ((response: any) => {
+        this.closecountArr = response.data;
+        this.closecountArr.forEach((ent: any) => {
+          if (ent.salescount != null) {
+            this.sclosecount = ent.salescount;
+          }
+          if (ent.reccount != null) {
+            this.rclosecount = ent.reccount;
+          }
+        });
+      })
+    );
+  }
 
 }
