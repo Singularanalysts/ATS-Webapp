8d0c721f7956bf1ae8acd42d5af9e54b2d2be22d
PraneethNarvee <praneethreddyjulakanti@narveetech.com>
2024-09-12 17:08:27 +0530

    tms comments api changes

--- src/app/taskmodule/comments/comments.component.ts
+++ src/app/taskmodule/comments/comments.component.ts
@@ -56,7 +56,7 @@ export class CommentsComponent {
     this.ticketid = this.data.ticketid || this.data.taskData.ticketid;
     const taskId = this.data.taskData?.taskid;
     const subtaskId = this.data.subtaskData?.subTaskId;
-    if(this.data.actionName === 'sub-task-comments') {
+    if(this.data.actionName === 'add-sub-task-comments') {
       this.subtaskServ.subtaskComments(subtaskId).subscribe({
         next: (response: any) => {
           this.comments = response.data;
@@ -83,50 +83,74 @@ export class CommentsComponent {
   }
 
   getUserInitials(fullname: string) {
-  
     const nameParts = fullname.split(' ');
-
     let initials = '';
-
     if (nameParts.length > 1) {
       initials = nameParts[0].charAt(0) + nameParts[1].charAt(0);
     } else {
       initials = nameParts[0].charAt(0) + (nameParts[0].charAt(1) || '');
     }
-
     return initials.toUpperCase();
   }
 
   addComment(): void {
     if (this.newComment.trim()) {
-      const commentData = {
-        comments: this.newComment,
-        taskid: this.data.subtaskData?.taskId || this.data.taskData?.taskid,
-        status: this.data.subtaskData?.status || this.data.taskData?.status,
-        ticketid: this.ticketid,
-        updatedby: localStorage.getItem('userid')
-      };
-      this.subtaskServ.addComments(commentData).subscribe({
-        next: (response: any) => {
-          if (response.status == 'success') {
-            this.dataToBeSentToSnackBar.message = 'Comments added successfully';
-            this.dialogRef.close();
-          } else {
-            this.dataToBeSentToSnackBar.message = response.message;
+      // Check if actionName equals 'add-task-comments'
+      if (this.data.actionName === 'add-task-comments') {
+        const taskCommentData = {
+          comments: this.newComment,
+          taskid: this.data.taskData?.taskid,
+          status: this.data.taskData?.status,
+          ticketid: this.ticketid,
+          updatedby: localStorage.getItem('userid')
+        };
+        this.taskServ.addComments(taskCommentData).subscribe({
+          next: (response: any) => {
+            if (response.status === 'success') {
+              this.dataToBeSentToSnackBar.message = 'Comments added successfully';
+              this.dialogRef.close();
+            } else {
+              this.dataToBeSentToSnackBar.message = response.message;
+            }
+            this.snackBarServ.openSnackBarFromComponent(this.dataToBeSentToSnackBar);
+          },
+          error: (err: any) => {
+            this.dataToBeSentToSnackBar.message = err.message;
+            this.dataToBeSentToSnackBar.panelClass = ['custom-snack-failure'];
+            this.snackBarServ.openSnackBarFromComponent(this.dataToBeSentToSnackBar);
           }
-          this.snackBarServ.openSnackBarFromComponent(this.dataToBeSentToSnackBar);
-
-        },
-        error: (err: any) => {
-          this.dataToBeSentToSnackBar.message = err.message;
-          this.dataToBeSentToSnackBar.panelClass = ['custom-snack-failure'];
-          this.snackBarServ.openSnackBarFromComponent(this.dataToBeSentToSnackBar);
-        }
-      })
+        });
+      } else {
+        const subtaskCommentData = {
+          comments: this.newComment,
+          subTaskId: this.data.subtaskData?.subTaskId,
+          status: this.data.subtaskData?.status,
+          ticketid: this.ticketid,
+          updatedby: localStorage.getItem('userid')
+        };
+        this.subtaskServ.addSubtaskComments(subtaskCommentData).subscribe({
+          next: (response: any) => {
+            if (response.status === 'success') {
+              this.dataToBeSentToSnackBar.message = 'Comments added successfully';
+              this.dialogRef.close();
+            } else {
+              this.dataToBeSentToSnackBar.message = response.message;
+            }
+            this.snackBarServ.openSnackBarFromComponent(this.dataToBeSentToSnackBar);
+          },
+          error: (err: any) => {
+            this.dataToBeSentToSnackBar.message = err.message;
+            this.dataToBeSentToSnackBar.panelClass = ['custom-snack-failure'];
+            this.snackBarServ.openSnackBarFromComponent(this.dataToBeSentToSnackBar);
+          }
+        });
+      }
+      
+      // Reset the new comment input field
       this.newComment = '';
     }
   }
-
+  
   onClose() {
     this.dialogRef.close();
   }
