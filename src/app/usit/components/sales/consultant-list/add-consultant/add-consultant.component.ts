fc6d86c9a71032e8aed64fffcfe953ae7e8c0d03
Lukkynarvee <laxman@narveetech.com>
2025-01-24 16:59:36 +0530

    Super Admin Sales List

--- src/app/usit/components/sales/consultant-list/add-consultant/add-consultant.component.ts
+++ src/app/usit/components/sales/consultant-list/add-consultant/add-consultant.component.ts
@@ -354,9 +354,8 @@ export class AddconsultantComponent implements OnInit, OnDestroy {
   initConsultantForm(consultantData: Consultantinfo) {
 
 
-    if( this.flag ==='DomRecruiting' || this.role==='Sales Executive' || this.role==='Recruiter' || this.role==='Super Administrator' || this.role==='Recruiting Manager'){
+    if( this.flag ==='DomRecruiting' || this.role==='Sales Executive' || this.role==='Recruiter' || this.role==='Recruiting Manager'){
 
-      
       this.consultantForm = this.formBuilder.group({
         consultantid: [consultantData ? consultantData.consultantid : ''],
         consultantno: [consultantData ? consultantData.consultantno : ''],
